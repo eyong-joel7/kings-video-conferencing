d77bf98a733a428fc1715af84bf5a59e4d927fdb
JOEL EYONG <62267332+eyong-joel7@users.noreply.github.com>
2021-10-20 09:24:28 +0300

    fix

--- src/components/Conferenceroom/conferenceRoomElements.js
+++ src/components/Conferenceroom/conferenceRoomElements.js
@@ -5,40 +5,42 @@ export const VideoContainer = styled.div`
     width: 100vw;
     margin-right: auto;
     margin-left: auto;
-     height: calc(100vh - 8rem);
+    height: 100%;
     justify-content: center;
     display: flex;
     overflow:hidden;
+    padding: 20px;
 `;
 
 export const VideoWrapper = styled.div`
   display: grid;
    max-height: 100%;
-   width: 100%;
-   padding: 10px;
-   grid-gap: 5%;
+   width: auto;
+   max-width:100%;
+   /* padding: 10px;
+   grid-gap: 5%; */
   grid-template-columns: 1fr 1fr;
   grid-template-rows: 1fr 1fr;
+  grid-gap: 10px; 
   justify-items: center;
   overflow:hidden;
- 
-  /* @media screen and (max-width: 1000px) {
-    grid-template-columns: repeat(auto-fit, minmax(40%, 100%));
-    grid-template-rows: repeat(auto-fit, minmax(50%, calc(100vh - 8rem)));
-  } */
+  @media screen and (max-width: 768px) {
+    grid-gap: 5px; 
+  }
 `;
 
 export const VideoCard = styled.div`
   display: flex;
   flex-direction: column;
-  max-width:100%;
+  width:100%;
   justify-content: flex-start;
   align-items: center; 
   border-radius: 5px;
- height:50%;
-  padding:0;
-  margin:0;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.5);
+  height:100%;
+  overflow: hidden;
+  padding:5px;
+  margin:5px;
+  border: 2px solid rgba(0, 0, 0, 0.5);
   transition: all 0.2s ease-in-out;
   &:hover {
     transform: scale(1.01);
@@ -46,8 +48,7 @@ export const VideoCard = styled.div`
 `;
 
 export const VideoStream = styled.video`
-   max-height: calc(100% - 50px);
-   max-width:100%;
+   height: 80%;
   -ms-transform:${({mirrorMode}) => mirrorMode ? 'scaleX(-1)': null};
   -moz-transform: ${({mirrorMode}) => mirrorMode ? 'scaleX(-1)': null};
   -webkit-transform: ${({mirrorMode}) => mirrorMode ? 'scaleX(-1)': null};
@@ -56,7 +57,7 @@ export const VideoStream = styled.video`
 
 export const FooterContainer = styled.div`
 display:flex;
-max-height:50px;
+max-height:20%;
 flex-wrap:nowrap;
 align-items: center;
 padding: 5px;
